8e42aa1d0ad9003e48f86d285f0797a4028b3a0f
陳業淵 <luke1987515@hotmail.com>
2018-08-17 17:30:36 +0800

    3.1. Variables and Mutability
    https://doc.rust-lang.org/book/2018-edition/ch03-01-variables-and-mutability.html

---
+++ variables/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const MAX_POINT: u32 = 100000;
+    println!("The value of x is: {}", MAX_POINT);
+
+    let y = 5;
+
+    let y = y + 1;
+
+    let y = y * 2;
+
+    println!("The value of y is: {}", y);
+}
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\Documents\GitHub\try_PWA\variables\target\debug\variables.pdb: C:\Users\Administrator\Documents\GitHub\try_PWA\variables\src\main.rs
