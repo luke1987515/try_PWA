12b0da669cf1a0256c1b8cabb3b1d8ce37ac7eaa
Yeh-Yuan Cheen <luke1987515@hotmail.com>
2018-08-24 10:58:31 +0800

    cargo clean

--- variables/target/debug/deps/variables-a1247e159a693766.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Administrator\Documents\GitHub\try_PWA\variables\target\debug\deps\variables-a1247e159a693766.exe: src\main.rs
-
-C:\Users\Administrator\Documents\GitHub\try_PWA\variables\target\debug\deps\variables-a1247e159a693766.d: src\main.rs
-
-src\main.rs:
--- variables/target/debug/variables-a1247e159a693766.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Administrator\Documents\GitHub\try_PWA\variables\target\debug\variables-a1247e159a693766.pdb: C:\Users\Administrator\Documents\GitHub\try_PWA\variables\src\main.rs
