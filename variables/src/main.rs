d1fa3539533d8dad276fcc214aad199e1fdaa3f9
陳業淵 <luke1987515@hotmail.com>
2018-08-24 10:42:05 +0800

    Data Types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
+    // 3.1. Variables and Mutablility
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
 
-    const MAX_POINT: u32 = 100000;
+    const MAX_POINT: u32 = 100_000;
     println!("The value of x is: {}", MAX_POINT);
 
     let y = 5;
@@ -14,4 +15,12 @@ fn main() {
     let y = y * 2;
 
     println!("The value of y is: {}", y);
+
+    let spaces = "   ";
+    println!("The value of spaces is: {}", spaces);
+    let spaces = spaces.len();
+    println!("The value of spaces is: {}", spaces);
+
+    // 3.2. Data Types
+
 }
--- variables/target/debug/deps/variables-b28e4912c55df67a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Administrator\Documents\GitHub\try_PWA\variables\target\debug\deps\variables-b28e4912c55df67a.exe: src\main.rs
-
-C:\Users\Administrator\Documents\GitHub\try_PWA\variables\target\debug\deps\variables-b28e4912c55df67a.d: src\main.rs
-
-src\main.rs:
