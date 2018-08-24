17e9248577ace4c31a608c8b5d1f2b966ecd346c
Yeh-Yuan Cheen <luke1987515@hotmail.com>
2018-08-24 10:36:44 +0800

    Numeric Operations

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -13,5 +13,34 @@ fn main() {
 
     let y = y * 2;
 
+
     println!("The value of y is: {}", y);
+    let z = 2.0; // f64
+
+    let a: f32 = 3.0; // f32
+
+    println!("The value of z is: {}", z);
+
+    println!("The value of a is: {}", a);
+
+    // addition
+    let sum = 5 + 10;
+    println!("The value of sum is: {}", sum);
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("The value of difference is: {}", difference);
+
+    // multiplication
+    let product = 4 * 30;
+    println!("The value of product is: {}", product);
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("The value of quotient is: {}", quotient);
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("The value of remainder is: {}", remainder);
+
 }
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Administrator\Documents\GitHub\try_PWA\variables\target\debug\variables.pdb: C:\Users\Administrator\Documents\GitHub\try_PWA\variables\src\main.rs
